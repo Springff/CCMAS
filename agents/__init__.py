0b1241752d9e7cd9ef42f13786501f058e3a5b61
Chunyang Chai <166700944+Springff@users.noreply.github.com>
2026-01-11 00:15:26 +0800

    Update __init__.py

--- agents/__init__.py
+++ agents/__init__.py
@@ -8,13 +8,11 @@ from .data_agent import DataAgent
 from .motif_agent import MotifAgent
 from .analysis_agent import AnalysisAgent
 from .bioknowledge_agent import BioKnowledgeAgent
-from .visualization_agent import VisualizationAgent
 
 __all__ = [
     "PlanAgent",
     "DataAgent",
     "MotifAgent",
     "AnalysisAgent",
-    "BioKnowledgeAgent",
-    "VisualizationAgent",
+    "BioKnowledgeAgent"
 ]
